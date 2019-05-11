4124cfc9cc68c06313559a257055eec5b35e2952
Nilyass <47905754+Nilyass@users.noreply.github.com>
2019-05-11 21:41:36 +0000

    Update app.js

--- app.js
+++ app.js
@@ -62,11 +62,11 @@ Client.on("message", async (message) => {
 		const embed = new Discord.RichEmbed()
 		.setColor(0x954D23)
 		.setTitle("Command List:")
-		.addField("!help", "Will give the current command list")
-		.addField("!ping", "WIll show the ping time for the bot")
-		.addField("!say [text]", "Will make the bot say something")
-		.addField("!announcement [text]", "Will make the bot say an announcement and tag everyone")
-		.addField("!cat", "Will send a random cat image");
+		.addField("+help", "Will give the current command list")
+		.addField("+ping", "WIll show the ping time for the bot")
+		.addField("+say [text]", "Will make the bot say something")
+		.addField("+announcement [text]", "Will make the bot say an announcement and tag everyone")
+		.addField("+cat", "Will send a random cat image");
 		message.channel.send({embed})
 	}
 
