a1fb805fae5fced751277a88336c1ac03e28f913
Nilyass <47905754+Nilyass@users.noreply.github.com>
2019-05-11 21:12:10 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,75 @@
+const Discord = require('discord.js');
+const superagent = require("superagent");
+const Client = new Discord.Client();
+const OwnerID = "537147937583529994";
+const prefix = "!"
+
+Client.on("ready", () => {
+	console.log("online");
+	Client.user.setPresence({ game: { name: `MTools`, type: 0} });
+});
+
+// welcome message
+
+
+Client.on("message", async (message) => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+	
+	let command = message.content.split(" ")[0];
+	command = command.slice(prefix.length);
+	
+	let args = message.content.split(" ").slice(1);
+	
+	if (command === "ping") {
+		message.channel.send(`Pong! Time took: ${Date.now() - message.createdTimestamp} ms`);
+	} else
+
+	if (command === "say") {
+		message.delete()
+                const embed = new Discord.RichEmbed()
+		.setColor("RANDOM")
+		.setDescription(args.join(" "));
+		message.channel.send({embed})
+	} else
+
+   if (command === "cat") {
+	   const { body } = await superagent
+	   .get('aws.random.cat/meow');
+	   const embed = new Discord.RichEmbed()
+	   .setColor(0x954D23)
+	   .setTitle("Meow :cat:")
+	   .setImage(body.file)
+	   message.channel.send({embed})
+   } else
+
+   if (command === "announcement") {
+	   if (message.member.hasPermission("ADMINISTRATOR")) {
+		   const color = args[0]
+		   const text = args.slice(1).join(" ");
+		   if (text.length < 1) return message.channel.send("Can not announce nothing");
+		   //const colour = args.slice(2).join("");
+		   const embed = new Discord.RichEmbed()
+		   .setColor("0x" + color)
+		   .setTitle("Important Announcement:")
+		   .setDescription(text);
+		   message.channel.send("@everyone")
+		   message.channel.send({embed})
+	   }
+   } else
+
+	if (command == "help") {
+		const embed = new Discord.RichEmbed()
+		.setColor(0x954D23)
+		.setTitle("Command List:")
+		.addField("!help", "Will give the current command list")
+		.addField("!ping", "WIll show the ping time for the bot")
+		.addField("!say [text]", "Will make the bot say something")
+		.addField("!announcement [text]", "Will make the bot say an announcement and tag everyone")
+		.addField("!cat", "Will send a random cat image");
+		message.channel.send({embed})
+	}
+
+});
+
+Client.login("NTc1MDczNzc2NTkzNjY2MDQ5.XNctTw.XIkGhErSt050WB8OFHRP3pDTnsk"); //replace with your token dont share yours.
