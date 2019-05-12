17753d205d02e3dbbc1f2cbf1f612ff8f723f730
Nilyass <47905754+Nilyass@users.noreply.github.com>
2019-05-12 15:24:07 +0000

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ Client.on("message", async (message) => {
 		   .setTitle("Important Announcement:")
 		   .setDescription(text);
 		   message.channel.send("@everyone")
-		   message.channel.send({})
+		   message.channel.send({text})
 	   }
    } else
 
